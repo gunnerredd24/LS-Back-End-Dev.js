940cfc8f58461347d90f679e53671f762cf69391
Gunner Redd <Gunnerredd27@gmail.com>
2024-09-28 17:06:04 -0600

    updated calculator and started mortgageCalculator

--- JS101-Basics/lesson_2/calculator.js
+++ JS101-Basics/lesson_2/calculator.js
@@ -18,58 +18,56 @@ function invalidNumber(num) {
 
 let again = 'y';
 
-while(again === 'y'){
-prompt(msgObject.Welcome);
+while (again === 'y') {
+  prompt(msgObject.welcome);
 
-prompt(msgObject.FirstNumber);
-let number1 = READLINE.question();
+  prompt(msgObject.firstNumber);
+  let number1 = READLINE.question();
 
-while (invalidNumber(number1)) {
-  prompt(msgObject.ValidNumber);
-  number1 = READLINE.question();
+  while (invalidNumber(number1)) {
+    prompt(msgObject.validNumber);
+    number1 = READLINE.question();
+  }
+
+  prompt(msgObject.secondNumber);
+  let number2 = READLINE.question();
+
+  while (invalidNumber(number2)) {
+    prompt(msgObject.validNumber);
+    number2 = READLINE.question();
+  }
+
+  prompt(msgObject.operation);
+  let operation = READLINE.question();
+
+  while (!['1' , '2' , '3' , '4'].includes(operation)) {
+    prompt(msgObject.operationCheck);
+    operation = READLINE.question();
+  }
+
+
+  switch (operation) {
+    case '1':
+      prompt(`The result of ${number1} + ${number2} is ${Number(number1) + Number(number2)}`);
+      break;
+    case '2':
+      prompt(`The result of ${number1} - ${number2} is ${Number(number1) - Number(number2)}`);
+      break;
+    case '3':
+      prompt(`The result of ${number1} * ${number2} is ${Number(number1) * Number(number2)}`);
+      break;
+    case '4':
+      prompt(`The result of ${number1} / ${number2} is ${Number(number1) / Number(number2)}`);
+      break;
+  }
+
+  prompt(msgObject.another);
+  again = READLINE.question().toLowerCase();
+
+  console.log('\x1Bc'); // This command will clear the console when running node.js (On most terminals)
 }
 
-prompt(msgObject.SecondNumber);
-let number2 = READLINE.question();
-
-while (invalidNumber(number2)) {
-  prompt(msgObject.ValidNumber);
-  number2 = READLINE.question();
-}
-
-prompt(msgObject.Operation);
-let operation = READLINE.question();
-
-while (!['1' , '2' , '3' , '4'].includes(operation)) {
-  prompt(msgObject.OperationCheck);
-  operation = READLINE.question();
-}
-
-
-switch (operation) {
-  case '1':
-    prompt(`The result of ${number1} + ${number2} is ${Number(number1) + Number(number2)}`);
-    break;
-  case '2':
-    prompt(`The result of ${number1} - ${number2} is ${Number(number1) - Number(number2)}`)
-    break;
-  case '3':
-    prompt(`The result of ${number1} * ${number2} is ${Number(number1) * Number(number2)}`)
-    break;
-  case '4':
-    prompt(`The result of ${number1} / ${number2} is ${Number(number1) / Number(number2)}`)
-    break;
-}
-
-prompt(msgObject.Another)
-again = READLINE.question().toLowerCase();
-
-console.log('\x1Bc'); // This command will clear the console when running node.js (On most terminals)
-}
-
-
-
-while(!['y' , 'n'].includes(again)) {
-  prompt(msgObject.AnotherCheck);
-  again = READLINE.question().toLowerCase()
+while (!['y' , 'n'].includes(again)) {
+  prompt(msgObject.anotherCheck);
+  again = READLINE.question().toLowerCase();
 }
