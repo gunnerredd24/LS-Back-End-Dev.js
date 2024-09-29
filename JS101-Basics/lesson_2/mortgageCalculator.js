7f2235c30dfcef5951a506e1490547c4ee1dbd16
Gunner Redd <Gunnerredd27@gmail.com>
2024-09-29 15:13:36 -0600

    editted mortgage calculator(almost finished)

---
+++ JS101-Basics/lesson_2/mortgageCalculator.js
@@ -0,0 +1,47 @@
+const READLINE = require('readline-sync');
+
+const consoleMessages = require('./mortgage_calculator_msgs.json');
+
+function msgDisplay(msg) {
+  console.log(`=> ${msg}\n`);
+}
+
+function invalidNumber(number) {
+  return number.trim === '' || Number.isNaN(Number(number));
+}
+
+msgDisplay(consoleMessages.welcome);
+
+msgDisplay(consoleMessages.loanAmount);
+
+let loanTotal = READLINE.question();
+
+while (invalidNumber(loanTotal)) {
+  msgDisplay(consoleMessages.validInput);
+  loanTotal = READLINE.question();
+}
+
+msgDisplay(consoleMessages.APR);
+let annualPercentageRate = READLINE.question();
+
+while (Number(annualPercentageRate) < 1) annualPercentageRate *= 10; // To convert any decimal inputs to integers.
+
+while (invalidNumber(annualPercentageRate)) {
+  msgDisplay(consoleMessages.APRCheck)
+  annualPercentageRate = READLINE.question();
+}
+
+msgDisplay(consoleMessages.loanDuration);
+
+let loanDuration = READLINE.question();
+
+while (invalidNumber(loanDuration)) {
+  msgDisplay(consoleMessages.loanDurationCheck)
+  loanDuration = READLINE.question();
+}
+
+console.log(loanDuration);
+
+console.log(annualPercentageRate);
+
+console.log(loanTotal);
