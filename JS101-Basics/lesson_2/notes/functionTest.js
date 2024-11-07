07dd3393e6c47f1c67c1b26b19aba138f15fe008
Gunner Redd <gunnerredd27@gmail.com>
2024-11-07 06:49:55 -0700

    took notes while using this for testing examples.

--- JS101-Basics/lesson_2/notes/functionTest.js
+++ JS101-Basics/lesson_2/notes/functionTest.js
@@ -1,19 +1,24 @@
-function changeName(name) {
-  name = "bob"; // does this reassignment change the variable outside the function?
-}
+// function changeName(name) {
+//   name = "bob"; // does this reassignment change the variable outside the function?
+// }
 
-function anotherFunction() {
-  let name = "jim";
-  changeName(name);
-  console.log(name); // => logs 'jim'
-}
+// function anotherFunction() {
+//   let name = "jim";
+//   changeName(name);
+//   console.log(name); // => logs 'jim'
+// }
 
-anotherFunction();
+// anotherFunction();
 
-function capitalize(names) {
-  return names.map(name => name[0].toUpperCase() + name.slice(1));
-}
+// function capitalize(names) {
+//   return names.map(name => name[0].toUpperCase() + name.slice(1));
+// }
 
-let names = ["chris", "kevin", "naveed"];
-capitalize(names); // returns ['Chris', 'Kevin', 'Naveed']
-console.log(names); // => ['chris', 'kevin', 'naveed']
+// let names = ["chris", "kevin", "naveed"];
+// capitalize(names); // returns ['Chris', 'Kevin', 'Naveed']
+// console.log(names); // => ['chris', 'kevin', 'naveed']
+
+let advice = "Few things in life are as important as house training your pet dinosaur.";
+console.log(advice.substring(0, advice.search('house')));
+// Expected return value:
+// => 'Few things in life are as important as '
